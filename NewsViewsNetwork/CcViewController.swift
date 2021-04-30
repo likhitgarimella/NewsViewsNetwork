0e55d6e14fa313b09c31b6a0427cbe1dc6520737
Likhit Garimella <likhitgmella@gmail.com>
2021-04-30 23:14:55 +0530

    web view code replicate in all 27 screens vcs

--- NewsViewsNetwork/CcViewController.swift
+++ NewsViewsNetwork/CcViewController.swift
@@ -6,13 +6,21 @@
 //
 
 import UIKit
+import WebKit
 
 class CcViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        let webview = WKWebView(frame: view.frame)
+        webview.translatesAutoresizingMaskIntoConstraints = false
+        webview.frame = CGRect(x: 0, y: 0, width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height)
+        view.addSubview(webview)
         
+        let url = URL(string: "https://www.newsviewsnetwork.com/india/")!
+        let request = URLRequest(url: url)
+        webview.load(request)
         
     }
     
